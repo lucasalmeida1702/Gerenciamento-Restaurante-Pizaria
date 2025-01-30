da16a202351dcdacbab714045b44e7426ebf94aa
lucasalmeida1702 <lucasalmeidasouza127@gmail.com>
2025-01-30 20:05:57 -0300

    Add files via upload

---
+++ struct_cardapio_1_1_item.js
@@ -0,0 +1,6 @@
+var struct_cardapio_1_1_item =
+[
+    [ "descricao", "struct_cardapio_1_1_item.html#adcb31d91dc32fc8b792b01297db9fc15", null ],
+    [ "nome", "struct_cardapio_1_1_item.html#a4cb41b0bea37ab98239106a5b03c8ac6", null ],
+    [ "preco", "struct_cardapio_1_1_item.html#a780183549ff908d6ae96499a1b81740f", null ]
+];
---
+++ struct_fornecedor_1_1_info.js
@@ -0,0 +1,7 @@
+var struct_fornecedor_1_1_info =
+[
+    [ "endereco", "struct_fornecedor_1_1_info.html#a08ac31c37780fed9c473baed4272a09e", null ],
+    [ "nome", "struct_fornecedor_1_1_info.html#aa3aeda4d61af5871f025fb1f2276119d", null ],
+    [ "produto", "struct_fornecedor_1_1_info.html#adf4dda8abd6195fa7ae6a6d0721b695b", null ],
+    [ "telefone", "struct_fornecedor_1_1_info.html#ae180d5f506ad39c81ee510ff83664dfb", null ]
+];
